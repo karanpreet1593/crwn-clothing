af854cb4ac98da1ee667f5b5f39a9b1d7373e0e8
Ranjeet Singh <PC@MacBook-Air.local>
2021-08-03 13:35:19 -0400

    update menu item to navigate with Router HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Route, Switch, } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
+const HatsPage =(props)=>{
+  console.log(props);
+  return(
+    <div>
+    <button onClick={()=> props.history.push('/')}   >home</button>
+  <h1>hatPage</h1>
+ 
+  </div>
+ 
+  );
+  
+  
+}
 
 function App() {
   return (
     <div >
-     <HomePage/>
+    <Switch>
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/shop/hats/' component={HatsPage} />
+    </Switch>
     </div>
   );
 }
